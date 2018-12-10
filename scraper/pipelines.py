440b8df4b52b6c6af033b1f299728d966198ad14
dmittor <43282595+dmittor@users.noreply.github.com>
2018-12-10 14:52:37 -0500

    z

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -11,17 +11,10 @@ from scrapy.exceptions import DropItem
 
 class DeDupePipeline(object):
     def __init__(self):
-        self.email_addresses_seen = set()
-        self.phone_numbers_seen = set()
 
     def process_item(self, item, spider):
         if item['email_address'] in self.email_addresses_seen:
-            raise DropItem("Duplicate email item found: %s" % item)
-        if item['phone_number'] in self.phone_numbers_seen:
-            raise DropItem("Duplicate phone item found: %s" % item)
-        if item['email_address'] not in self.email_addresses_seen:
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
             self.email_addresses_seen.add(item['email_address'])
-            return item
-        if item['phone_number'] not in self.phone_numbers_seen:
-            self.phone_numbers_seen.add(item['phone_number'])
             return item
