f3e9bd9bad9e6fe808b304988b784cef850568dc
dmittor <43282595+dmittor@users.noreply.github.com>
2018-12-10 22:26:22 -0500

    z

--- scraper/spiders/thorough_spider.py
+++ scraper/spiders/thorough_spider.py
@@ -73,7 +73,7 @@ class ThoroughSpider(scrapy.Spider):
             item['field'] = found_address
             yield item
 
-        for found_phone in selector.re('(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?'):
+        for found_phone in selector.re('(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4})'):
             item2 =  EmailAddressItem()
             item2['field'] = found_phone
             yield item2
