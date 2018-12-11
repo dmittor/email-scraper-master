56635936c82f3a9b47203489fa105981c91eb1a8
dmittor <43282595+dmittor@users.noreply.github.com>
2018-12-10 20:02:34 -0500

    z

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -17,11 +17,11 @@ class DeDupePipeline(object):
     def process_item(self, item, spider):
         if item['email_address'] in self.email_addresses_seen:
             raise DropItem("Duplicate email address found: %s" % item)
-        elif item['phone_number'] in self.phone_numbers_seen:
+        if item['phone_number'] in self.phone_numbers_seen:
             raise DropItem("Duplicate email address found: %s" % item)
-        elif item['phone_number'] not in self.phone_numbers_seen:
+        if item['phone_number'] not in self.phone_numbers_seen:
             self.email_addresses_seen.add(item['phone_number'])
             return item
-        elif item['email_address'] not in self.email_addresses_seen:
+        if item['email_address'] not in self.email_addresses_seen:
             self.email_addresses_seen.add(item['email_address'])
-            return item
+        return item
