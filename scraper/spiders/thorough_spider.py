67a6ece07db845e866ba19d7c91e1d283acf43ee
dmittor <43282595+dmittor@users.noreply.github.com>
2018-12-12 12:20:52 -0500

    z

--- scraper/spiders/thorough_spider.py
+++ scraper/spiders/thorough_spider.py
@@ -68,13 +68,10 @@ class ThoroughSpider(scrapy.Spider):
         # into <a> elements but rather as clickable span elements - e.g. jana.com
         all_urls.update(selector.re('"(\/[-\w\d\/\._#?]+?)"'))
 
-        for tr in sel.xpath('//table[@class="main entry"]/tr'):
+        for found_address in selector.re('(\d{3}[-\.\s]\d{3}[-\.\s]\d{4}|\(\d{3}\)\s\d{3}[-\.\s]\d{4})'):
             item = EmailAddressItem()
-            item['companyName'] = tr.xpath('td[1]//a/text()').extract()[0].strip()
-            item['contactName'] = tr.xpath('td[1]//a/text()').extract()[0].strip()
-            item['category'] = tr.xpath('td[1]//a/text()').extract()[0].strip()
-            item['phone'] = tr.xpath('td[1]//a/text()').extract()[0].strip()
-            item['website'] = tr.xpath('td[1]//a/text()').extract()[0].strip()
+            item['field'] = found_address
+            item['source_url'] = response.url
             yield item
 
 
